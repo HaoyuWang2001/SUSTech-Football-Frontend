9c94891a60adae215f6c132021fe4cced5d1dc82
Jianyang Guo <12111506@mail.sustech.edu.cn>
2026-03-11 22:33:15 +0800

    [feat] 新增通知模块组件
    将通知模块封装为组件，便于后期修改

---
+++ wechat-miniprogram/components/notification-block/notification-block.js
@@ -0,0 +1,68 @@
+// components/notification-block.js
+Component({
+
+  options: {
+    styleIsolation: 'apply-shared'
+  },
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+    title: String,
+
+    visible: {
+      type: Boolean,
+      value: true
+    },
+
+    open: {
+      type: Boolean,
+      value: false
+    },
+
+    list: {
+      type: Array,
+      value: []
+    },
+
+    hint: String,
+
+    emptyText: String,
+
+    height: {
+      type: String,
+      value: '200px'
+    }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+    toggle() {
+      this.triggerEvent('toggle')
+    },
+
+    itemClick(e) {
+
+      const item = e.currentTarget.dataset.item
+
+      this.triggerEvent('itemclick', {
+        item: item
+      })
+
+    }
+
+  }
+})
